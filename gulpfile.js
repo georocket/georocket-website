7ea4c2cec6f746bd05a411649b2e9d9320aead62
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2015-11-06 14:52:59 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+var bower = require("gulp-bower");
+var connect = require("connect");
+var compress = require("compression");
+var del = require("del");
+var fs = require("fs");
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var path = require("path");
+var prettyHrtime = require("pretty-hrtime");
+var serveStatic = require("serve-static");
+
+var Metalsmith = require("metalsmith");
+var define = require("metalsmith-define");
+var markdown = require("metalsmith-markdown");
+var sass = require("metalsmith-sass");
+var templates = require("metalsmith-templates");
+
+var bowerrc = JSON.parse(fs.readFileSync("./.bowerrc"));
+
+var paths = {
+  site: "site",
+  src: "src"
+};
+
+function build(done, dev) {
+  var canonicalUrl = "http://georocket.io";
+  var siteUrl = canonicalUrl;
+  if (dev) {
+    siteUrl = "http://localhost:4000";
+  }
+
+  Metalsmith(__dirname)
+    // configure Metalsmith
+    .source(paths.src)
+    .destination(paths.site)
+
+    // define global metadata
+    .use(define({
+      site: {
+        url: siteUrl,
+        canonicalurl: canonicalUrl,
+        time: Date.now()
+      }
+    }))
+
+    // apply template engine to markdown files only (apply in-place)
+    .use(templates({
+      engine: "nunjucks",
+      pattern: "**/*.md",
+      inPlace: true
+    }))
+
+    // convert markdown to HTML
+    .use(markdown({
+        // use smart typographic punctuation for quotes, dashes, etc.
+        smartypants: true
+    }))
+
+    // apply template engine to all files
+    .use(templates({
+      engine: "nunjucks"
+    }))
+
+    // convert scss to css
+    .use(sass({
+        includePaths: [
+            path.join(bowerrc.directory, "bootstrap/scss")
+        ]
+    }))
+
+    // build site
+    .build(done);
+}
+
+gulp.task("bower", function() {
+  return bower();
+});
+
+gulp.task("build", ["bower"], function(done) {
+  build(done);
+});
+
+gulp.task("buildDev", ["bower"], function(done) {
+  build(done, true);
+});
+
+gulp.task("watch", ["buildDev"], function() {
+  // start web server
+  var app = connect();
+  app.use(compress());
+  app.use(serveStatic(paths.site, {
+    "index": ["index.html", "index.htm"]
+  }));
+  app.listen(4000, function() {
+    gutil.log("Listening on port", gutil.colors.cyan("4000"), "...");
+  });
+
+  return gulp.watch([path.join(paths.src, "**", "*")], {}, function() {
+    gutil.log("Rebuilding ...");
+    var start = process.hrtime();
+    build(function() {
+      gutil.log("Finished", "'" + gutil.colors.cyan("rebuilding") + "'",
+          "after", gutil.colors.magenta(prettyHrtime(process.hrtime(start))));
+    }, true);
+  });
+});
+
+gulp.task("clean", function(cb) {
+  del([bowerrc.directory, paths.site], cb);
+});
+
+gulp.task("default", ["build"]);
